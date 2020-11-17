14ece54bc7ae9014b2e6389732665413606518cb
DeanLaz <DeanLazx@gmail.com>
2020-11-16 20:33:50 -0800

    Authentication Intiial Commit

--- PlacesX-Client/src/App.js
+++ PlacesX-Client/src/App.js
@@ -10,6 +10,7 @@ import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import UserPlaces from "./places/pages/UserPlaces";
+import Auth from "./user/pages/Auth";
 function App() {
   return (
     <Router>
@@ -27,6 +28,9 @@ function App() {
           </Route>
           <Route path="/places/:placeId">
             <UpdatePlace />
+          </Route>
+          <Route path="/auth">
+            <Auth />
           </Route>
           <Redirect to="/" />
         </Switch>
---
+++ PlacesX-Client/src/user/pages/Auth.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Auth.css";
+import Card from "../../shared/components/UIElements/Card";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hooks";
+// import PlaceList from "../../places/components/PlaceList";
+const Auth = () => {
+  const [formState, inputHandler] = useForm({
+    email: {
+      value: "",
+      isValid: false,
+    },
+    password: {
+      value: "",
+      isValid: false,
+    },
+  });
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className="authentication">
+      <h2>Login Required!</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        <Input
+          element="input"
+          id="email"
+          type="email"
+          label="E-Mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please Enter Valid Email!"
+          onInput={inputHandler}
+        />
+        <Input
+          element="input"
+          id="password"
+          type="password"
+          label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please Enter Valid Password! (5 Character Minimum)"
+          onInput={inputHandler}
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
+};
+
+export default Auth;
